84b61e4c0efd8efe08a960af67387bcf876bb6a8
EvgenVls <evgenvl@ukr.net>
2024-01-04 19:52:30 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const sortByDescendingFriendCount = users => {
-    
-}
+const sortByDescendingFriendCount = (users) => {
+  return users.toSorted((a, b) => b.friends.length - a.friends.length)
+};
 
 console.log(
   sortByDescendingFriendCount([
